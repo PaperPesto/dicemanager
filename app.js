c893e06e0247e4d46d7bfe54b7466ed2ca0cb463
DESKTOP-KF4SFLC\Leonardo <leonardo.bianchini90@gmail.com>
2018-05-30 19:25:29 +0200

    app.js funzionante

--- app.js
+++ app.js
@@ -3,10 +3,8 @@ console.log('Starting app.js');
 
 const yargs = require('yargs');
 
-const diceManager = require('./diceResult.js');
 const diceManager = require('./diceManager.js');
 
-const argv = yargs.argv;
 const argv = yargs
 .command('roll', 'Roll a dice', {
     number:{
@@ -23,16 +21,13 @@ const argv = yargs
 .help()
 .argv;
 
-var ndice = argv.ndice;
-var facedice = argv.facedice;
 var ndice = argv.number;
 var facedice = argv.faces;
 
-console.log('You roll', ndice, "d" + facedice);
 console.log('You rolled', ndice, "d" + facedice);
 var results = diceManager.getResults(ndice, facedice);
-console.log(JSON.stringify(results));
+//console.log(JSON.stringify(results));
 
 //console.log(JSON.stringify(results));
 var distributionResults = diceManager.testUniformDistribution(ndice, facedice);
-console.log(distributionResults);
+console.log("Distribution:", distributionResults);
